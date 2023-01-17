7712523bbaa1700531b7be6b090e5a7895b90eb7
Martien van den Akker <martien.van.den.akker@oracle.com>
2023-01-17 09:47:35 +0100

    rename node to kubelet-node

--- Udemy/KubernetesCertifiedAdministrator/tls/gen_kubelet_node01.sh
+++ Udemy/KubernetesCertifiedAdministrator/tls/gen_kubelet_node01.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname $0)
 #
-NAME=node01
-SUBJECT="/CN=system:node:$NAME/O=system:nodes"
+NODE=node01
+NAME="kubelet-$NODE"
+SUBJECT="/CN=system:node:$NODE/O=system:nodes"
 #
 $SCRIPTPATH/gen_key.sh $NAME 
 $SCRIPTPATH/gen_csr.sh $NAME "$SUBJECT"
