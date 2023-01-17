d09cdb6f8ef0a5250a7877f33014d4de5e6779a3
Martien van den Akker <martien.van.den.akker.oracle.com>
2023-01-17 09:36:19 +0100

    Update CNF to working version.

--- Udemy/KubernetesCertifiedAdministrator/tls/gen_apiserver.sh
+++ Udemy/KubernetesCertifiedAdministrator/tls/gen_apiserver.sh
@@ -2,7 +2,7 @@
 SCRIPTPATH=$(dirname $0)
 #
 NAME=apiserver
-SUBJECT="//CN=kube-apiserver"
+SUBJECT="/CN=kube-apiserver"
 #
 $SCRIPTPATH/gen_key.sh $NAME 
 $SCRIPTPATH/gen_csr_conf.sh $NAME "$SUBJECT"
